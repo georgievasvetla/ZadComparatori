6feee67ab7f234e51a7a64a36268357beda9097d
svetla.g <svetlana.georgieva@edu1.pgzaimov.com>
2023-02-21 09:00:42 +0200

    first commit

---
+++ Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad11
+{
+    internal class Book:IComparable<Book>
+    {
+         
+            public Book(string title, int year)
+            {
+                this.Title = title;
+                this.Year = year;
+                
+            }
+            public string Title { get;  set; }
+            public int Year { get;  set; }
+
+        public int CompareTo(Book other)
+        {
+            return Year.CompareTo(other.Year);
+        }
+
+        public override string ToString()
+            {
+                return string.Format("{0} - {1}", this.Title, this.Year);
+            }
+
+           
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zad11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2013);
+            Book bookTwo = new Book("The Documents in the Case", 2007);
+            Book bookThree = new Book("The Documents in the Case", 2012);
+            List<Book> book = new List<Book>()
+            {
+                bookOne,
+                bookTwo,
+                bookThree
+
+            };
+            book.Sort();
+            book.ForEach(x => Console.WriteLine(x.Year));
+
+        }
+    }
+}
